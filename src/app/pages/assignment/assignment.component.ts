838797bf7d9e149c01b87b086eb9533332f4fec0
jonathanfalcon95 <jonathan.falcon@tmconsulting.mx>
2019-08-20 19:00:11 -0400

    changes to assignment

--- src/app/pages/assignment/assignment.component.ts
+++ src/app/pages/assignment/assignment.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,TemplateRef } from '@angular/core';
+import { HttpClient, HttpHeaders } from "@angular/common/http";
+import { BehaviorSubject } from "rxjs";
+
+import { BsModalService, BsModalRef } from "ngx-bootstrap/modal";
+import { GlobalService } from "../providers/global.service";
 
 @Component({
   selector: 'app-assignment',
@@ -7,9 +12,171 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AssignmentComponent implements OnInit {
 
-  constructor() { }
 
-  ngOnInit() {
+  ngOnInit(){
+    this.getAss();
+}
+
+
+title = 'AngularCRUDExample';
+UserList: any;
+HardwareList: any;
+SoftwareList: any;
+AssList : any;
+assignment:any;
+bsModalRef: BsModalRef;
+user: any;
+titleModal: string="";
+save: boolean=false;
+edit: boolean=false;
+constructor(private globalService: GlobalService, private bsModalService: BsModalService) {
+   this.user=[];
+   this.assignment=[];
+   this.AssList=[];
+}
+OpenUserModal(template: TemplateRef<any>, option, index:number) {
+  this.user=[]
+  if(option==="save"){
+    this.titleModal='Assignment';
+    this.save=true;
+  }else
+  if(option==="edit"){
+    this.titleModal='Edit User';
+    this.edit=true;
+    console.log(this.UserList[index])
+    this.user=this.UserList[index];
+    console.log(this.user);
+  }else
+  if(option==='delete'){
+    this.user=this.UserList[index];
+
   }
+  this.bsModalRef = this.bsModalService.show(template);
+  
+}
+
+getAss() {
+    this.globalService.getModel("/users").then(
+        result => {
+          console.log(result);
+          this.UserList = result;
+        },
+        err => {
+          console.log(err);
+          //this.loader.dismiss();
+        }
+      );
+
+
+      this.globalService.getModel("/hardware").then(
+        result => {
+          console.log(result);
+          this.HardwareList= result;
+        },
+        err => {
+          console.log(err);
+          //this.loader.dismiss();
+        }
+      );
+      this.globalService.getModel("/software").then(
+        result => {
+          console.log(result);
+          this.SoftwareList = result;
+        },
+        err => {
+          console.log(err);
+          //this.loader.dismiss();
+        }
+      );
+}
+
+showAss(){
+console.log("done");
+  this.globalService.getModel("/assignment/user/"+this.assignment.UserID).then(
+    result => {
+      console.log(result);
+      this.AssList= result;
+    },
+    err => {
+      console.log(err);
+      //this.loader.dismiss();
+    }
+  );
+
+
+}
+
+deleteUser() {
+  this.globalService.removeModel(this.user.id,"/users").then(
+    result => {
+      console.log(result);
+      this.getAss();
+    },
+    err => {
+      console.log(err);
+      
+      //this.loader.dismiss();
+    }
+  );
+  
+  this.onClose()
+}
+
+editAss() {
+  console.log(this.user)
+  
+  let postUser = {
+    'id': this.user.id,
+    'userName': this.user.userName,
+    'Name': this.user.name,
+    'lastName': this.user.lastName,
+    'Age': this.user.age,
+    'lastSessionDateTime': '2019-08-05T15:02:29.393'
+  };
+
+  this.globalService.updateModel(this.user.id,postUser, "/users").then(
+    result => {
+      console.log(result);
+      this.getAss();
+    },
+    err => {
+      console.log(err);
+      //this.loader.dismiss();
+    }
+  );
+ 
+  this.onClose()
+}
+
+
+saveAss() {
+  console.log(this.assignment)
+  
+  let postAss = {
+   "userID": this.assignment.UserID,
+   "softwareID": this.assignment.SoftwareID,
+   "hardwareID": this.assignment.HardwareID,
+  };
+console.log(postAss);
+
+  this.globalService.addModel(postAss, "/assignment").then(
+    result => {
+      console.log(result);
+      this.getAss();
+    },
+    err => {
+      console.log(err);
+    }
+  );
+ 
+  this.onClose();
+}
+
+onClose() {
+  this.edit=false;
+  this.save=false;
+  this.bsModalRef.hide();
+}
+
 
 }
